00f9350e740024fae0c1130b47e2b87a2ed1e5b3
Ruslan Simchuk <simadovakin999@gmail.com>
2021-05-15 00:57:34 +0300

    Setup db, create task model and do migration

--- firstapp/app.py
+++ firstapp/app.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+# this model of task for task manager
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Task {self.id}>"
+
 
 @app.route("/")
 def index():
