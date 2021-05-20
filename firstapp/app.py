b1e0e2d952f107651b2af746b542684c625f5d29
SimaDovakin <simadovakin999@gmail.com>
2021-05-20 16:25:53 +0300

    Add task adding functionality

--- firstapp/app.py
+++ firstapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -19,7 +19,20 @@ class Task(db.Model):
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        task_content = request.form['task']
+        task = Task(content=task_content)
+
+        try:
+            db.session.add(task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was some error!"
+
+    else:
+        tasks = Task.query.order_by(Task.created_at).all()
+        return render_template('index.html', tasks=tasks)
 
 @app.route("/users/<name>")
 def user(name):
